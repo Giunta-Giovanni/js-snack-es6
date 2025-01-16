98f4d1971f1ab2df1a9c8bc847829579b93b1792
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-16 16:13:06 +0100

    [feat] human code snack-1

--- js/snack-1.js
+++ js/snack-1.js
@@ -2,3 +2,11 @@
 Snack 1 
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 */
+
+// creo un array di oggetti bidimensionale
+    // negli oggetti inserisco le Proprieta: nome e peso con i loro valori
+    // creo un ciclo di tutti gli oggetti
+        // mi salvo la proprietà peso e creo una condizione
+        // se il valore tra quelli che abbiamo è il piu piccolo
+            //stampamelo a schermo
+ 
